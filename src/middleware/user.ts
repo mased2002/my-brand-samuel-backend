bcdafcb1b7433c1487115876f495ae61215aa4dd
mased2002 <edsam2302@gmial.com>
2024-02-28 19:17:34 +0200

    bcrypt

--- src/middleware/user.ts
+++ src/middleware/user.ts
@@ -1,2 +1,42 @@
 import { compare, hash } from "bcrypt";
-import { Jwt } from "jsonwebtoken";
+import Joi, { ValidationResult } from "joi";
+import * as jwt  from "jsonwebtoken";
+import { Request, Response, NextFunction } from "express";
+
+export function isLoggedIn(){
+    
+}
+
+const registerSchema = Joi.object({
+    name: Joi.string().min(3).required(),
+    email: Joi.string().email().required(),
+    password: Joi.string().min(6).required(),
+    username: Joi.string().required()
+})
+
+const loginSchema = Joi.object({
+    email: Joi.string().email().required(),
+    password: Joi.string().min(6).required()
+})
+
+export  function validateRegistiration(req: Request, res: Response , next: NextFunction){
+    const validationResult: ValidationResult = registerSchema.validate(req.body)
+
+    if (validationResult.error){
+        return res.status(400).json({error: validationResult.error.details[0].message})
+    }
+    // if error is not there
+
+    next();
+}
+
+export  function validateLogin(req: Request, res: Response, next: NextFunction){
+    const validationResult: ValidationResult = loginSchema.validate(req.body)
+
+    if(validationResult.error){
+        return res.status(400).json({error: validationResult.error.details[0].message});
+    }
+    // if error is not there
+
+    next();
+}
--- src/routes/Users.ts
+++ src/routes/Users.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { userControl } from "../controllers/User";
+import { validateLogin } from "../middleware/user";
 
 const userRoute = Router()
 
 userRoute.post("/signup", userControl.createUser);
 userRoute.get("/getAll", userControl.getAllUsers);
+userRoute.post("/login", validateLogin,userControl.loginUser);
 userRoute.delete("/:email/delete", userControl.deleteUser);
 
 export default userRoute;
