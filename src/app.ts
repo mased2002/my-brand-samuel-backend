7a3e7c1244acad8b9648f406e4da32efbe5f8fb8
mased2002 <edsam2302@gmial.com>
2024-03-14 19:28:27 +0200

    app.ts trying to configure cors

--- src/app.ts
+++ src/app.ts
@@ -11,15 +11,8 @@ const app: Application = express();
 
 
 // app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cors(
-    {
-        origin: "*",
-        methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-        credentials: true,
-        optionsSuccessStatus: 204
-    }
-));
 app.use(express.json());
+app.use(cors());
 app.use(express.urlencoded({extended: true}));
 
 
