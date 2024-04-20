dc35d1dd97c9387b002279b383f2ac2efcf5d0c1
mased2002 <edsam2302@gmial.com>
2024-04-20 18:50:36 +0200

    still trying the cors thing

--- src/app.ts
+++ src/app.ts
@@ -14,21 +14,21 @@ const app: Application = express();
 // app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors());
-const corsOpts = {
-    origin: '*',
-    methods: [
-    'GET',
-    'POST',
-    'DELETE',
-    'PATCH',
-    'PUT'
-    ],
-    allowedHeaders: [
-    'Content-Type',
-    'Authorization',
-    ],
-    };
-app.use(cors(corsOpts));
+// const corsOpts = {
+//     origin: '*',
+//     methods: [
+//     'GET',
+//     'POST',
+//     'DELETE',
+//     'PATCH',
+//     'PUT'
+//     ],
+//     allowedHeaders: [
+//     'Content-Type',
+//     'Authorization',
+//     ],
+//     };
+// app.use(cors(corsOpts));
 app.use(morgan('dev'));
 // app.use(express.urlencoded({extended: true}));
 
